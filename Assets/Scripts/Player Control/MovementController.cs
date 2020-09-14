02dcdf938b8708d46a0284409f67dd0ed8506420
Alex Brendel <alexbrendel@gmx.net>
2020-09-14 23:21:44 +0200

    fixed movement controller jumping and wallclipping

--- Assets/Scripts/Player Control/MovementController.cs
+++ Assets/Scripts/Player Control/MovementController.cs
@@ -31,16 +31,18 @@ public class MovementController : MonoBehaviour {
     private static readonly Quaternion planeNormalRotation = Quaternion.Euler(0, 0, -90);
     private Vector3 plane;
 
-    private void Update() {
+    private void FixedUpdate() {
         Vector3 pos = rb.transform.position;
 
         // Smooth input velocity
         velocity = Mathf.SmoothDamp(velocity, inputVelocity, ref velocityChangeRate, velocitySmooth);
-        Vector3 v = Vector3.right * velocity * maxVelocity * Time.deltaTime;
+        Vector3 v = Vector3.right * velocity * maxVelocity * Time.fixedDeltaTime;
         //Vector3 plane;
 
+        RaycastHit hit;
+
         // Get ground Vector and project velocity accordingly
-        if (Physics.Raycast(pos + feetOffset, Vector3.down, out RaycastHit hit, 0.1f)) {
+        if (Physics.Raycast(pos + feetOffset, Vector3.down, out hit, 0.1f)) {
             if (hit.normal == Vector3.up) {
                 plane = Vector3.right;
             }
@@ -50,15 +52,23 @@ public class MovementController : MonoBehaviour {
             v = Vector3.Project(v, plane);
         }
 
+        // Check Wall Collision
+        float mag = v.magnitude;
+        if (Physics.Raycast(pos, v, out hit, 0.8f)) {
+            v *= Mathf.Max(0, Mathf.Min(mag, hit.distance - 0.5f)) / mag;
+        }
+
         // Move player
         rb.MovePosition(pos + v);
     }
 
     private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(rb.transform.position + feetOffset, 0.1f);
     }
 
     private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.magenta;
         Gizmos.DrawLine(rb.transform.position + feetOffset, rb.transform.position + feetOffset + plane);
     }
 }
