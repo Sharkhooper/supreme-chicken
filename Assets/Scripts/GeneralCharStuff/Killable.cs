db86432877a5376e3dd7070bef009ebc7b040757
Sven D <dyck.sven@gmail.com>
2020-09-15 13:24:25 +0200

    killable
    basic stuff

---
+++ Assets/Scripts/GeneralCharStuff/Killable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Killable : MonoBehaviour
+{
+
+    private GameObject _character;
+
+    private void Awake()
+    {
+        _character = gameObject;
+    }
+
+    public void GetKilled()
+    {
+        if (_character.CompareTag("Player"))
+        {
+           // SceneManager.LoadScene("Game");  // Lädt die InGame Szene neu
+        }
+        else
+        {
+            Destroy(_character);
+        }
+    }
+}
