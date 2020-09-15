85a5273c096cd6f132a860382b99045f361c32d2
Sven D <dyck.sven@gmail.com>
2020-09-16 01:17:07 +0200

    basic attack & dash
    .

---
+++ Assets/Scripts/Player Control/DashAbility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DashAbility : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody _rB;
+    [SerializeField] private float _dashCD = 0.5f; //in seconds
+    private static float currentCD = 0f;
+    
+
+    public void HandleDashInput(InputAction.CallbackContext context)
+    {
+        Debug.Log("Dashing!");
+        if (currentCD.Equals(0f))
+        {
+            currentCD += _dashCD;
+            // dash
+        }
+    }
+}
---
+++ Assets/Scripts/Player Control/PlayerAttack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private GameObject _player;
+
+    [SerializeField] private float attackRadius = 1.0f;
+    [SerializeField] private Vector3 attackPosition = Vector3.right; // wird auf Player transform addiert
+    [SerializeField] private int enemyLayer = 1;
+    [SerializeField] private float attackCD = 0.5f;
+    
+    private void Awake()
+    {
+        _player = gameObject;
+    }
+    
+    
+
+    public void HandleAttackInput(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Attacking");
+        Collider[] hits = Physics.OverlapSphere(_player.transform.position + attackPosition,attackRadius);
+        if (hits.Length > 0)
+        {
+            foreach (var h in hits)
+            {
+                GameObject tempGO = h.gameObject; // temporary GameObject
+                if (tempGO.layer.Equals(enemyLayer))
+                {
+                    tempGO.GetComponent<Killable>().GetKilled();
+                }
+            }
+        }
+    }
+}
