6264188905647d37a0ea4a60475ae51cd5f648ff
Sven D <dyck.sven@gmail.com>
2020-09-16 11:50:41 +0200

    Attack completed

--- Assets/Scripts/Player Control/PlayerAttack.cs
+++ Assets/Scripts/Player Control/PlayerAttack.cs
@@ -11,27 +11,40 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] private float attackRadius = 1.0f;
     [SerializeField] private Vector3 attackPosition = Vector3.right; // wird auf Player transform addiert
     [SerializeField] private int enemyLayer = 1;
-    [SerializeField] private float attackCD = 0.5f;
+    [SerializeField] private float attackCD = 3.0f;
+    private float currentCD = 0f;
     
     private void Awake()
     {
         _player = gameObject;
     }
-    
-    
+
+    private void Update()
+    {
+        currentCD -= Time.deltaTime;
+        if (currentCD < 0f)
+        {
+            currentCD = 0f;
+        }
+    }
+
 
     public void HandleAttackInput(InputAction.CallbackContext ctx)
     {
-        Debug.Log("Attacking");
-        Collider[] hits = Physics.OverlapSphere(_player.transform.position + attackPosition,attackRadius);
-        if (hits.Length > 0)
+        if (currentCD == 0f)
         {
-            foreach (var h in hits)
+            currentCD = attackCD;
+            Collider[] hits = Physics.OverlapSphere(_player.transform.position + attackPosition, attackRadius);
+            if (hits.Length > 0)
             {
-                GameObject tempGO = h.gameObject; // temporary GameObject
-                if (tempGO.layer.Equals(enemyLayer))
+                foreach (var h in hits)
                 {
-                    tempGO.GetComponent<Killable>().GetKilled();
+                    GameObject tempGO = h.gameObject; // temporary GameObject
+                    if (tempGO.layer.Equals(enemyLayer))
+                    {
+                        tempGO.GetComponent<Killable>().GetKilled();
+                        Debug.Log("killed something");
+                    }
                 }
             }
         }
