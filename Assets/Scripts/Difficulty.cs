5d69d6959b55eb70b076c0a32d007943eb358b22
Alex Brendel <alexbrendel@gmx.net>
2020-09-18 11:41:55 +0200

    Added difficulty base

---
+++ Assets/Scripts/Difficulty.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Difficulty : ScriptableObject {
+    public static Difficulty current;
+
+    [System.Serializable]
+    public struct Waiter {
+        public float range;
+        public float attackSpeed;
+    }
+
+    public Waiter waiter;
+}
