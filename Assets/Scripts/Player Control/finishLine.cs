d37d7d44210ac3e724788822794d460fde65bb77
Marvin Baumann <marvin_baumann@hotmail.com>
2020-09-18 14:19:57 +0200

    Added TMPro & WinScreen

--- Assets/Scripts/Player Control/finishLine.cs
+++ Assets/Scripts/Player Control/finishLine.cs
@@ -3,16 +3,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.InputSystem;
 
 public class finishLine : MonoBehaviour
 {
-
     [SerializeField] private int playerLayer = 10;
+    public int waitUntilReload;
+    public GameObject wonText, timerText;
+    public bool finished;
+
+    private float time;
+    void Start()
+    {
+        time = 0;
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == playerLayer)
+        if (other.gameObject.layer == playerLayer && !finished)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            StartCoroutine(Winning(other.gameObject));
         }
+    }
+
+    private IEnumerator Winning(GameObject player)
+    {
+        player.GetComponentInParent<PlayerInput>().enabled = false;
+        finished = true;
+        wonText.SetActive(true);
+        timerText.SetActive(true);
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time % 60);
+        int milli = Mathf.FloorToInt((time * 100) % 100);
+        string timeString = String.Format("{0:00}:{1:00}.{2:00}", minutes, seconds, milli);
+        timerText.GetComponent<TextMeshProUGUI>().text = "Your Time: " + timeString;
+        yield return new WaitForSeconds(waitUntilReload);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
