f208ab1349819b4ff08f40354ee6500d7d2a0b8d
Alex Brendel <alexbrendel@gmx.net>
2020-09-20 00:42:41 +0200

    Added music fucking finally

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,67 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class MusicController : MonoBehaviour {
+    private static MusicController active;
+    public static MusicController Active => active;
+
+    [SerializeField] private float fadeTime;
+    [SerializeField] private AudioSource intro;
+    [SerializeField] private AudioSource loop;
+
+    private Sequence sequence;
+    private float defaultVolume;
+    [SerializeField] private float lowVolume;
+
+    private void Awake() {
+        if (active == null) {
+            active = this;
+        }
+        else {
+            Destroy(gameObject);
+            return;
+        }
+
+        active = this;
+        DontDestroyOnLoad(gameObject);
+        defaultVolume = loop.volume;
+    }
+
+    private void OnDestroy() {
+        if (active == this) {
+            active = null;
+        }
+    }
+
+    public void PlayOrFadeIn() {
+        Debug.Log("Svenja steht auf richtig dicke Informatikerpimmel");
+        if (loop.isPlaying) {
+            if (sequence != null && sequence.IsPlaying()) {
+                sequence.Kill();
+            }
+            sequence = DOTween.Sequence();
+            sequence.Append(loop.DOFade(defaultVolume, fadeTime));
+            sequence.Play();
+        }
+        else {
+            double time = AudioSettings.dspTime + 0.3d;
+            intro.PlayScheduled(time);
+            double duration = (double)intro.clip.samples / intro.clip.frequency;
+            loop.PlayScheduled(time + duration);
+        }
+    }
+
+    public void FadeOut() {
+        if (loop.isPlaying) {
+            if (sequence != null && sequence.IsPlaying()) {
+                sequence.Kill();
+            }
+            sequence = DOTween.Sequence();
+            sequence.Append(loop.DOFade(lowVolume, fadeTime));
+            sequence.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/MusicEvent.cs
@@ -0,0 +1,32 @@
+﻿using DG.Tweening;
+using EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicEvent : MonoBehaviour
+{
+    [SerializeField] private float delay = 0;
+    private bool done = false;
+
+    private void Start() {
+        MainEventSystem.MainEvents.onGameStarts += StartMusic;
+    }
+
+    private void StartMusic() {
+        if (done) return;
+        done = true;
+        if (delay > 0) {
+            Sequence s = DOTween.Sequence();
+            s.AppendInterval(delay);
+            s.onComplete += () => {
+                MusicController.Active.PlayOrFadeIn();
+            };
+            s.Play();
+        }
+        else {
+            MusicController.Active.PlayOrFadeIn();
+        }
+    }
+
+}
